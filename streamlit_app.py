e001d1e355ddfea619797fa95a542aa30fdd48c0
zhanghui_china <63148804+zhanghui-china@users.noreply.github.com>
2024-04-23 09:53:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ def get_qa_chain(question:str,zhipuai_api_key:str):
 # Streamlit 应用程序界面
 def main():
     st.title('🦜🔗 动手学大模型应用开发')
-    zhipuai_api_key = st.sidebar.text_input('ChatGLM API Key', type='password')
+    #zhipuai_api_key = st.sidebar.text_input('ChatGLM API Key', type='password')
 
     # 添加一个选择按钮来选择不同的模型
     #selected_method = st.sidebar.selectbox("选择模式", ["qa_chain", "chat_qa_chain", "None"])
